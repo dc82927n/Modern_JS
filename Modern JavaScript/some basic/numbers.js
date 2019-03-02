30016d541cd17331fce6c40709428598dd1f0d1e
David Chan <dc82927n@gmail.com>
2019-03-02 17:08:44 -0500

    Add files via upload

---
+++ Modern JavaScript/some basic/numbers.js
@@ -0,0 +1,34 @@
+let num1 = 4;
+let num2 = 12
+let add = num1 + num2;
+let sub = num1 - num2;
+let div = num2 / num1;
+let div1 = num1 / num2;
+let mult = num1 * num2; 
+
+console.log(add);
+console.log(sub); 
+console.log(div);
+console.log(div1);
+console.log(mult);
+
+/* 
+student score challenge
+student score = 18
+maxscore = 20
+percent 90 
+
+*/
+
+
+
+
+let studentScore = 18; 
+let maxScore = 20; 
+let percentScore = studentScore / maxScore
+
+console.log(percentScore);
+
+
+
+
---
+++ Modern JavaScript/some basic/string.js
@@ -0,0 +1,21 @@
+let name = "david chan"; // we just created my name with a string 
+
+    console.log(name);
+    
+
+let firstName = "David"; 
+let lastName = "Chan";
+
+console.log(firstName + " " +  lastName);
+
+let city = "new york;" 
+let country = "usa";
+let location = city + " " + country;
+
+console.log (location); 
+
+
+
+
+
+
