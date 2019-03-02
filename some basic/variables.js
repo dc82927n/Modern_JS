73d47e56e393cac08799acd84c30d4f0c5420c0b
David <dc82927n@gmail.com>
2019-03-02 18:15:44 -0500

    added variable files

---
+++ some basic/variables.js
@@ -0,0 +1,52 @@
+/*
+3 variables rules
+1.you can't duplicated variables names: 
+2.you can't have number in variables example. 1,2,3,4,5
+3.you can use variable name that are keywords. 
+4. code is read from top to down. 
+*/ 
+
+
+// calculate celsius and sotre in celsius variable 
+// print that value
+
+// c = k - 273.15
+
+// k to celcius
+let k = 10;
+let c = k - 273.15
+
+// fahrenheit to celcius
+let f = 45 
+let celsius = (((f - 32) * 5) /9)
+
+
+
+console.log(" k to celcius " + c);  
+console.log(" fahrenheit to celcius " + celsius); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
