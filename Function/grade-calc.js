e202ea93c57e1b23d07e751a16abebfa1ac0c6c2
David <dc82927n@gmail.com>
2019-03-09 16:40:01 -0500

    learn some returns function concepts

--- Function/grade-calc.js
+++ Function/grade-calc.js
@@ -29,10 +29,27 @@ grade()
 //console.log(total);
 
 
+let gradeCalc = function(score, totalScore){
+	let percent = (score / totalScore) * 100; 
+	let letterGrade = ""; 
 
 
+	if(percent >= 90){
+		return letterGrade = "A" // be mindful of the return statement. it ends the excution when it's 
+							// the desire output is reached. so if you put return letterGrade if
+								// the output is reach ti's excuted. 
+	} else if (percent >= 80){
+		return letterGrade = "b"
+	}
+	
+	return percent, letterGrade; 
 
+	// another way using the backtick string
+	//return `you got a ${letterGrade}, (${percent}%)!`; 
+}
 
+let result = gradeCalc(85, 100)
+console.log(result)
 
 
 
