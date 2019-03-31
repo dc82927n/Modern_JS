e9d54db63eb77d15221a73bd8dba652359c8bd1c
David <dc82927n@gmail.com>
2019-03-31 10:15:35 -0400

    string method and num method

---
+++ Objects/number methods.js
@@ -0,0 +1,24 @@
+let num = 103.941
+
+console.log(num.toFixed(2));
+// output 103.94
+
+
+console.log(Math.round(num));
+//output 104
+console.log(Math.floor(num));
+// 
+console.log(Math.ceil(num)); 
+
+let min = 0 
+let max = 1
+
+let randomNum = Math.floor(Math.random(56) * (max - min + 1)) + min 
+console.log(randomNum)
+
+//challenge 
+
+
+
+
+
--- Objects/string-methods.js
+++ Objects/string-methods.js
@@ -1,6 +1,6 @@
 //string-methods.js
 
-let name = 'Andrew Mead'
+let name = ' Andrew Mead'
 
 console.log(name.length); 
 
@@ -10,8 +10,52 @@ console.log(name.toUpperCase());
 console.log(name.toLowerCase()); 
 
 
-// 
-console.log(name.includes("Andrew")); 
+//it check if each of char is in the string. if so it will create 
+// true. 
+console.log(name.includes("aM")); 
+
+
+console.log(name.includes(''))
+
+
+// we can try to create a stronger password
+
+let password = ("abc1234pass098")
+let tr = 'password'
+console.log("line 25")
+console.log(password.includes(`${tr}`))
+
+
+// name.trim will trim the white space in the front and end. 
+console.log(name.trim())
+
+
+// challenge create invalidPassword
+// length is more than 8  - and it doesn't contain the word password
+
+let check = function (str){
+	if (str.length < 8){
+		return false 
+		}else if (str.includes("password")){
+			return false
+		}
+		else {
+		return true
+		}
+	}
+	
+
+	
+
+console.log(check("frekrjpassworddfsddfcd"))
+
+
+
+
+
+
+
+
 
 
 
