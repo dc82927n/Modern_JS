33e399aad158d85ac3d3c35fbdf3252520fe2dd6
David <dc82927n@gmail.com>
2019-03-30 20:04:05 -0400

    obj challenge

--- Objects/objNmethods.js
+++ Objects/objNmethods.js
@@ -1,13 +1,40 @@
+
+
+
 let restaurant = {
     name: 'ASB', 
     guestCapacity: 75, 
     guestCount: 0, 
     checkAvailability: function (partySize){
-        console.log(partySize)
+    	//console.log(this)
+    	//console.log(true)
+    	
+    	let seatsleft = this.guestCapacity - this.guestCount;
+    	
+    	return partySize <= seatsleft;
+    }, 
+
+    seatParty: function (partySize){
+    	this.guestCount = this.guestCount + partySize 
+    },
+    removeParty: function(partySize){
+    	this.guestCount = this.guestCount - partySize
     }
 }
 
-restaurant.checkAvailability(4);
+//console.log(restaurant.checkAvailability(74));
+
+
+
+// seatParty add to the seat party. 
+// removeParty
+
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(15))
+restaurant.removeParty(10)
+console.log(restaurant.checkAvailability(12))
+
+//console.log(restaurant.checkAvailability(4)) 
 
 
 
---
+++ Objects/string-methods.js
@@ -0,0 +1,19 @@
+//string-methods.js
+
+let name = 'Andrew Mead'
+
+console.log(name.length); 
+
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase()); 
+
+
+// 
+console.log(name.includes("Andrew")); 
+
+
+
+
+
