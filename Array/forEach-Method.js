f2d22153f07e28ab895e1ec6ed0cb51e59eed338
David <dc82927n@gmail.com>
2019-03-31 22:27:19 -0400

    forEach method

---
+++ Array/forEach-Method.js
@@ -0,0 +1,29 @@
+// const notes = ['Note 1', "note 2", "note 3"]
+
+
+
+// notes[2] = "hello worlds"
+
+// notes.forEach(function(item){
+// 	console.log(item)
+// }); 
+
+// we can also include the index number as well example below: 
+
+
+
+const notes = ['Note 1', "note 2", "note 3"]
+
+
+
+notes[2] = "hello worlds"
+
+notes.forEach(function(item, ind){
+	
+	console.log(item, ind)
+}); 
+
+
+
+
+
---
+++ Array/forEach-challenge.js
@@ -0,0 +1,19 @@
+
+
+const todo = ["apple", "orange", "pin", "dog", 1, 3, 6, "cat", "bird", "sing", "bling"];
+
+	console.log(`you have ${todo.length} on the list:`)
+
+
+todo.forEach(function(list, index){
+	console.log(`${index + 1} ${list}`)
+
+})
+
+
+
+
+
+
+
+
